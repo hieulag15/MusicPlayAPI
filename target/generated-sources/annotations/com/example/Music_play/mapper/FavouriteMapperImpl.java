e3067ad22d5c91746298d41e95ea93eca9fa22c4
hieulag15 <hieuthanhtran12@gmail.com>
2024-03-27 01:20:15 +0700

    dockerfile config

--- target/generated-sources/annotations/com/example/Music_play/mapper/FavouriteMapperImpl.java
+++ target/generated-sources/annotations/com/example/Music_play/mapper/FavouriteMapperImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-25T22:02:26+0700",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20.0.2 (Oracle Corporation)"
+    date = "2024-03-27T01:09:57+0700",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
 )
 @Component
 public class FavouriteMapperImpl implements FavouriteMapper {
